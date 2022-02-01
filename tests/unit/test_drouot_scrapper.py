a379ec06491101caa11f46b2f855e4df94dd8ce7
Adrien MORVAN <amorvan@molotov.tv>
2022-02-01 09:33:45 +0100

    Update mock for result page

--- tests/unit/test_drouot_scrapper.py
+++ tests/unit/test_drouot_scrapper.py
@@ -10,12 +10,12 @@ class TestDroutScrapper(TestCase):
     def setUpClass(cls) -> None:
         cls.scrapper = GazetteDrouotScraper(token=None)
 
-    def test(self):
+    def test_result_parse(self):
         with mock.patch.object(self.scrapper, "url_to_soup", new=drouot_query_multiplexer):
             rez = self.scrapper.parse_result_page("test")
             self.assertGreater(len(rez), 0)
 
-    def test(self):
+    def test_catalog_parse(self):
         with mock.patch.object(self.scrapper, "url_to_soup", new=drouot_query_multiplexer):
             rez = self.scrapper.parse_catalog_page("test", 0)
             self.assertGreater(len(rez), 0)
