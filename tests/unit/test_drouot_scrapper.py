a87e33d670d0f9ec752b8b1953338859b2dd9d2c
Adrien MORVAN <amorvan@molotov.tv>
2022-02-03 15:03:02 +0100

    Improve test

--- tests/unit/test_drouot_scrapper.py
+++ tests/unit/test_drouot_scrapper.py
@@ -15,7 +15,12 @@ class TestDroutScrapper(TestCase):
             rez = self.scrapper.parse_result_page("test")
             self.assertGreater(len(rez), 0)
 
+            self.assertSetEqual({int(e["lot_id"]) for e in rez}, {5, 40, 53, 65, 67})
+            self.assertSetEqual({int(e["price"]) for e in rez}, {625, 100, 250, 150})
+
     def test_catalog_parse(self):
         with mock.patch.object(self.scrapper, "url_to_soup", new=drouot_query_multiplexer):
             rez = self.scrapper.parse_catalog_page("test", 0)
             self.assertGreater(len(rez), 0)
+
+            self.assertTrue(any("Non Communiqué" not in e["estimation"] for e in rez))
